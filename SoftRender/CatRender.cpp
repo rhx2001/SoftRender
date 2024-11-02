a00acdfd67addbc30dcb301e0f42146de7b17d55
rhx2001 <473670005@qq.com>
2024-11-02 18:30:49 +0800

    增加数学库

---
+++ SoftRender/CatRender.cpp
@@ -0,0 +1,10 @@
+#include"MathV.hpp"
+#include"MathM.hpp"
+int main() {
+	Vector<int, 2> a({ 1,2 });
+	Vector<int, 2> b({ 3,4 });
+	Vector<int, 3> c = {1,2,3};
+	Matrix<int, 2, 2> m = { {1,2},{3,4} };
+	Matrix<int, 2, 2> t = { {1,2},{4,5} };
+	std::cout << a<<"\n"<<a-b<<"\n"<<a+b<<"\n"<<a*b<<"\n"<<a.dot(b)<<"\n"<<c<<"\n"<<m+t ;
+}
